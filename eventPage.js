ed362b0a865a76264d419952d58ddb762627717f
Justin Asfour <justin.asfour0@gmail.com>
2018-01-26 16:35:33 -0500

    Version alpha complete

--- eventPage.js
+++ eventPage.js
@@ -1,16 +1,17 @@
 "use strict"
 
+var price = 0;
+
 function setTitle() {
-	var price = Tronix.getPrice("CAD");
 	chrome.browserAction.setTitle({
-		"title": ["1 TRX = ", price, " CAD"].join("")
+		"title": ["1 TRX = ", price.toFixed(5), " CAD"].join("")
 	})
 }
 
 var AJAX = {
 	get: function(url) {
 		return new Promise((resolve, reject) => {
-			const req = new XHRHttpRequest();
+			const req = new XMLHttpRequest();
 			req.onreadystatechange = (e) => {
 				if (req.readyState === 4) {
 					req.status === 200 ? resolve(req.response) : reject(req.status);
@@ -21,4 +22,38 @@ var AJAX = {
 			req.send();
 		})
 	}
+}
+
+function setBadge() {
+	chrome.browserAction.setBadgeText({
+		text: price.toFixed(2)
+	})
+}
+
+var Tronix = {
+	getPrice: async function(currency) {
+		try {
+			const response = await AJAX.get(["https://api.coinmarketcap.com/v1/ticker/tronix/?convert=", currency].join(""));
+			const trxData = JSON.parse(response)[0];
+			return parseFloat(trxData.price_cad);
+		} catch (e) {
+			console.error(e);
+		}
+	}
+}
+
+window.addEventListener("load", loadBackground());
+var fetchInterval;
+async function loadBackground() {
+    chrome.browserAction.setBadgeBackgroundColor({
+    	color: '#000000'
+	});
+	chrome.browserAction.setBadgeText({
+    	text: '...'
+	});
+
+
+    price = await Tronix.getPrice('CAD');
+    setTitle();
+    setBadge();
 }
